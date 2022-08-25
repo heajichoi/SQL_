8ed3c1ec07349ba85ba115db1bac261fc5fea87d
heajichoi <95556405+heajichoi@users.noreply.github.com>
2022-08-25 16:29:41 +0900

    Add files via upload

---
+++ sqlite1.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts(
+email TEXT,
+count INTEGER)
+''')
